9228c366ad15ad6582793dba90570403fbba7970
longlongway <longlongway2012@hotmail.com>
2018-11-28 14:33:03 +0800

    modify web style

--- js/comm.js
+++ js/comm.js
@@ -32,3 +32,46 @@ function frameLoadSub2(frameid,file,offsetHeight) {
     frame.src=file;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////
+
+function setAuthorization(Leaflet,map)
+{
+   
+    var tileAddress = 'https://api.mapbox.com/styles/v1/yqcim/';
+    tileAddress += 'cizh1ma3400ez2so5x1anhuzo/tiles/256/{z}/{x}/{y}';
+    tileAddress += '?access_token=pk.eyJ1IjoieXFjaW0iLCJhIjoiY2l6ZmhnZjEx';
+    tileAddress += 'MDBhajJ4cGxnNGN5MnhpdCJ9.pcZtdfk8mSFboCdwqkvW6g';
+    
+    var attribution = 'Map data &copy; <a href="http://openstreetmap.org">';
+    attribution += 'OpenStreetMap</a> contributors, ';
+    attribution += '<a href="http://creativecommons.org/licenses/by-sa/2.0/">';
+    attribution += 'CC-BY-SA</a>, ';
+    attribution += 'Imagery © <a href="http://mapbox.com">Mapbox</a>';
+
+    
+    Leaflet.tileLayer(tileAddress, {
+        maxZoom: 18,
+        attribution: attribution,
+        id: 'mapbox.streets'
+        }).addTo(map);
+}
+
+function getTileAddress()
+{
+    // var tileAddress = 'https://api.mapbox.com/styles/v1/yqcim/';
+    // tileAddress += 'cizh1ma3400ez2so5x1anhuzo/tiles/256/{z}/{x}/{y}';
+    // tileAddress += '?access_token=pk.eyJ1IjoieXFjaW0iLCJhIjoiY2l6ZmhnZjEx';
+    // tileAddress += 'MDBhajJ4cGxnNGN5MnhpdCJ9.pcZtdfk8mSFboCdwqkvW6g';
+    var tileAddress = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+    return tileAddress;
+}
+
+function getAttribution()
+{
+    var attribution = 'Map data &copy; <a href="http://openstreetmap.org">';
+    attribution += 'OpenStreetMap</a> contributors, ';
+    attribution += '<a href="http://creativecommons.org/licenses/by-sa/2.0/">';
+    attribution += 'CC-BY-SA</a>, ';
+    attribution += 'Imagery © <a href="http://mapbox.com">Mapbox</a>';
+    
+    return attribution;
+}
